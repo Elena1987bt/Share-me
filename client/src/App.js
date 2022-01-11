01fe10382b5df747d996dcc311b5e93aa581d5db
Elena1987bt <elena.ackovska24@gmail.com>
2022-01-11 13:38:32 +0100

    Added routes to the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './container/Home';
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+      ;
+    </>
+  );
 }
 
 export default App;
